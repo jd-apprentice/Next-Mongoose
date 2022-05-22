75358b038b0379202c0502dd49a47d9829979d84
jd-apprentice <emeraldusk@gmail.com>
2022-05-22 18:12:42 -0300

    added eslint

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+      domains: ["upload.wikimedia.org", "images.unsplash.com", "pbs.twimg.com", "static.zerochan.net"],
+    },
+  };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,12 +20,6 @@ function MyApp({ Component, pageProps }: App) {
             <a>Add Pet</a>
           </Link>
         </div>
-
-        <img
-          id="title"
-          src="https://upload.wikimedia.org/wikipedia/commons/1/1f/Pet_logo_with_flowers.png"
-          alt="pet care logo"
-        ></img>
       </div>
       <div className="grid wrapper">
         <Component {...pageProps} />
