0077e460e36de789c6bc52b3ced28f117f0e8ab3
jd-apprentice <emeraldusk@gmail.com>
2022-07-02 16:32:26 -0300

    serverSideProps

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 module.exports = {
-    reactStrictMode: true,
-    swcMinify: true,
-    images: {
-      domains: ["upload.wikimedia.org", "images.unsplash.com", "pbs.twimg.com", "static.zerochan.net", "cdn.discordapp.com"],
-    },
-  };
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: [
+      "upload.wikimedia.org",
+      "images.unsplash.com",
+      "pbs.twimg.com",
+      "static.zerochan.net",
+      "cdn.discordapp.com",
+      "s1.zerochan.net",
+    ],
+  },
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -50,7 +50,7 @@ const Index = ({ pets }: Pets) => (
 );
 
 /* Retrieves pet(s) data from mongodb database */
-export async function getStaticProps() {
+export async function getServerSideProps() {
   await dbConnect();
 
   /* find all the data in our database */
