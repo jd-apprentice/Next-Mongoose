f35547ec0e87a51d0d660997fc2e2365dea759e7
jd-apprentice <emeraldusk@gmail.com>
2022-07-02 16:12:22 -0300

    cdr discord

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ module.exports = {
     reactStrictMode: true,
     swcMinify: true,
     images: {
-      domains: ["upload.wikimedia.org", "images.unsplash.com", "pbs.twimg.com", "static.zerochan.net"],
+      domains: ["upload.wikimedia.org", "images.unsplash.com", "pbs.twimg.com", "static.zerochan.net", "cdn.discordapp.com"],
     },
   };
